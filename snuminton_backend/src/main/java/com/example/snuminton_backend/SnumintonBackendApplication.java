f7e08779ab04cf2e6ebb0fdcdc6ee33732f3cf50
sungjin20 <msjkim2200@naver.com>
2025-09-22 23:43:20 +0900

    [ADD] 초기 프로젝트 구성

---
+++ snuminton_backend/src/main/java/com/example/snuminton_backend/SnumintonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.snuminton_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnumintonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnumintonBackendApplication.class, args);
+    }
+
+}
